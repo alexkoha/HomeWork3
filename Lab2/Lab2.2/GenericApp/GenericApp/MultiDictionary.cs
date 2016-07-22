02f256dc308b8c6a2c0c9c56c4404a496b8f2ad6
alexkoha <alexkoha@gmail.com>
2016-07-22 21:27:00 +0300

    Add files via upload

---
+++ Lab2/Lab2.2/GenericApp/GenericApp/MultiDictionary.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApp
+{
+    public class MultiDictionary<K, V> : IMultiDictionary<K, V>, IEnumerable<KeyValuePair<K, IEnumerable<V>>>
+            where K : struct
+            where V : new()
+    {
+        private Dictionary<K, LinkedList<V>> Dic = new Dictionary<K, LinkedList<V>>();
+
+        public ICollection<K> Keys => Dic.Keys;
+
+        public ICollection<V> Values
+        {
+            get
+            {
+                ICollection<V> collect = new List<V>();
+                foreach (var key in Dic.Keys)
+                {
+                    foreach (var val in Dic[key])
+                    {
+                        collect.Add(val);
+                    }
+                }
+                return collect;
+            }
+        }
+
+        public int Count => Dic.Keys.Sum(key => Dic[key].Count);
+
+        public void Add(K key, V value)
+        {
+            if (!Attribute.IsDefined(typeof(K), typeof(KeyAttribute)))
+            {
+                throw new ArgumentException("Key must be KeyAttribute", nameof(key));
+            };
+
+            if (Dic.ContainsKey(key))
+            {
+                Dic[key].AddLast(value);
+            }
+            else
+            {
+                Dic[key] = new LinkedList<V>();
+                Dic[key].AddLast(value);
+            }
+        }
+
+        public bool Remove(K key)
+        {
+            var containsKey = Dic.ContainsKey(key);
+            if (!containsKey) return false;
+            Dic[key].Clear();
+            return true;
+        }
+
+        public void CreateNewValue(K key)
+        {
+            var v = new V();
+            Add(key, v);
+        }
+
+        public bool Remove(K key, V value)
+        {
+            return Dic.ContainsKey(key) && Dic[key].Remove(value);
+        }
+
+        public void Clear()
+        {
+            Dic.Clear();
+        }
+
+        public bool ContainsKey(K key)
+        {
+            return Dic.ContainsKey(key);
+        }
+
+        public bool Contains(K key, V value)
+        {
+            if (Dic.ContainsKey(key))
+            {
+                foreach (var val in Dic[key])
+                {
+                    if (Equals(val, value)) return true;
+                }
+            }
+            return false;
+        }
+
+        public IEnumerator<KeyValuePair<K, IEnumerable<V>>> GetEnumerator()
+        {
+            var newList = new List<KeyValuePair<K, IEnumerable<V>>>();
+            foreach (var item in Dic)
+            {
+                newList.Add(new KeyValuePair<K, IEnumerable<V>>(item.Key, item.Value));
+            }
+            return newList.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Lab2/Lab2.2/GenericApp/GenericApp/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace GenericApp
+{
+    [Key]
+    public struct Shelf
+    {
+        private int _numberOfShelf;
+
+        public Shelf(int numberOfShelf)
+        {
+            _numberOfShelf = numberOfShelf;
+        }
+    }
+
+    public struct NoneKeyAttribute
+    {
+       
+    }
+
+    public class Package
+    {
+        private int _numberOfPackage;
+        private string _nameOgPackage;
+
+        public Package(int number, string name)
+        {
+            _numberOfPackage = number;
+            _nameOgPackage = name;
+        }
+
+        public Package()
+        {
+        }
+
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var test = new MultiDictionary<Shelf, Package>();
+            var test2 = new MultiDictionary<NoneKeyAttribute, Package>();
+            var test3 = new MultiDictionary<int, Package>();
+
+            Console.WriteLine("Test 1 : Goot parameters");
+            var _newShelf1 = new Shelf(53);
+            var _newShelf2 = new Shelf(23);
+            var _newShelf3 = new Shelf(12);
+
+            test.Add(_newShelf1, new Package(345, "Bamboo"));
+            test.Add(_newShelf1, new Package(231, "Bamboo"));
+            test.Add(_newShelf2, new Package(132,"Bamboo"));
+            test.Add(_newShelf3, new Package(12, "Mobile"));
+            test.Add(_newShelf3, new Package(567, "Mobile"));
+
+            Console.WriteLine("We expected tp 3 keys , lets print keys.");
+            test.Keys.ToList().ForEach(shelf => Console.WriteLine(shelf));
+            Console.WriteLine($"We expected tp 5 values , lets print count : {test.Count}");
+
+            Console.WriteLine("\nTest 2 : Bed parameters");
+            Console.WriteLine("Now we expected Exception.");
+
+            try
+            {
+                test2.Add(new NoneKeyAttribute(), new Package(567, "Mobile"));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            Console.WriteLine("\nTest 3 : Bed parameters");
+            Console.WriteLine("Now we expected Exception.");
+            try
+            {
+                test3.Add(123, new Package(567, "Mobile"));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
